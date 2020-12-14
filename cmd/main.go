d10b9953bdc7d959aa49b7a8e6da37a1bbf445c5
Nappy-Says <chokolatapapito@gmail.com>
2020-12-14 23:44:12 +0500

    .................blt

--- cmd/main.go
+++ cmd/main.go
@@ -2,57 +2,62 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
-	"os"
-	"time"
-	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/Nappy-Says/crud/cmd/app"
 	"github.com/Nappy-Says/crud/pkg/customers"
 	"github.com/Nappy-Says/crud/pkg/security"
+//second pack
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4/pgxpool"
+//r_uber
 	"go.uber.org/dig"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+const (
+	HOST = "0.0.0.0"
+	PORT = "9999"
 )
 
 func main() {
-	host := "0.0.0.0"
-	port := "9999"
-	dbConnectionString := "postgres://app:pass@localhost:5432/db"
-	if err := execute(host, port, dbConnectionString); err != nil {
-		log.Print(err)
+	dsn := "postgres://app:pass@localhost:5432/db"
+	if err := execute(HOST, PORT, dsn); err != nil {
 		os.Exit(1)
 	}
 }
 
-func execute(host, port, dbConnectionString string) (err error) {
-
-	dependencies := []interface{}{
+func execute(server, port, dsn string) (err error) {
+	deps := []interface{}{
+		app.NewServer,
+		mux.NewRouter,
+		customers.NewService,
+		security.NewService,
+		func() (*pgxpool.Pool, error) {
 			connCtx, _ := context.WithTimeout(context.Background(), time.Second*5)
-			return pgxpool.Connect(connCtx, dbConnectionString)
+			return pgxpool.Connect(connCtx, dsn)
 		},
+		func(serverHandler *app.Server) *http.Server {
 			return &http.Server{
-				Addr:    host + ":" + port,
-				Handler: server,
+				Addr:    net.JoinHostPort(server, port),
+				Handler: serverHandler,
 			}
 		},
 	}
 
 	container := dig.New()
-	for _, v := range dependencies {
-		err = container.Provide(v)
+	for _, dep := range deps {
+		err = container.Provide(dep)
 		if err != nil {
 			return err
 		}
 	}
 
-	err = container.Invoke(func(server *app.Server) {
-		server.Init()
-	})
+	err = container.Invoke(func(app *app.Server) { app.Init() })
 	if err != nil {
 		return err
 	}
 
-	return container.Invoke(func(server *http.Server) error {
-		return server.ListenAndServe()
-	})
+	return container.Invoke(func(s *http.Server) error { return s.ListenAndServe() })
 }
