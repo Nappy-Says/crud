95ca3d9ced7c966d4ea591d6f744289dd93261e7
NaSa <chokolatapapitp@gmail.com>
2020-12-21 14:01:00 +0500

    32 security

--- pkg/security/service.go
+++ pkg/security/service.go
@@ -22,3 +22,77 @@ func (s *Service) Auth(login, password string) bool {
 	}
 	return true
 }
+
+
+func (s *Service) TokenForCustomer(ctx context.Context, phone, password string) (string, error) {
+
+	var hash string
+	var id int64
+
+	err := s.db.QueryRow(
+		ctx,
+		"SELECT id, password FROM customers WHERE phone = $1",
+		phone).Scan(&id, &hash)
+	if err == pgx.ErrNoRows {
+
+		return "", ErrNoSuchUser
+	}
+
+	if err != nil {
+
+		return "", ErrInternal
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+
+		return "", ErrInvalidPassword
+	}
+
+	buffer := make([]byte, 256)
+	n, err := rand.Read(buffer)
+	if n != len(buffer) || err != nil {
+
+		return "", ErrInternal
+	}
+
+	token := hex.EncodeToString(buffer)
+	_, err = s.db.Exec(
+		ctx,
+		"INSERT INTO customers_tokens (token, customer_id) VALUES ($1, $2)",
+		token, id)
+	if err != nil {
+
+		return "", ErrInternal
+	}
+
+	return token, nil
+}
+
+func (s *Service) AuthenticateCustomer(ctx context.Context, token string) (int64, error) {
+	var id int64
+	var expire time.Time
+	err := s.db.QueryRow(
+		ctx,
+		"SELECT customer_id, expire FROM customers_tokens WHERE token=$1",
+		token).Scan(&id, &expire)
+	if err == pgx.ErrNoRows {
+
+		return 0, ErrNoSuchUser
+	}
+
+	if err != nil {
+
+		return 0, ErrInternal
+	}
+
+	timeNow := time.Now().Format("2020-07-07 10:36:21")
+	timeEnd := expire.Format("2020-07-07 10:36:21")
+
+	if timeNow > timeEnd {
+
+		return 0, ErrExpireToken
+	}
+
+	return id, nil
+}
